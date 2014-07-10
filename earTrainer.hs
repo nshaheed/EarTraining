92e1dd794bb7b4015623e4d116127da26d39f579
Nicholas Shaheed <nshaheed@sulawesi.ittc.ku.edu>
2014-07-10 16:44:40 -0500

    added interval guess comparison

--- earTrainer.hs
+++ earTrainer.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
---writeMidi
 module Main where
 
 import Codec.Midi
@@ -20,28 +19,39 @@ looper context (x',y') (a,b) pn = do
         (a',b') <- getNotes pn (a,b)
         let intButtIdx = intToPitchToButtIdx (a',b')
             pn'        = isButtPressed context (x',y')
-            
+            usrButtIdx = getButtPressed context (x',y')
+            guessed    = if isNothing usrButtIdx then False else True
+
         when pn' $ playNotes (a',b')
         --pn' <- send context $ 
 
         putStrLn ""
+        if guessed
+           then if (abs (intButtIdx) == ((floor $ fromJust usrButtIdx)::Int)) then putStrLn "Correct Guess"
+                                                                              else putStrLn "Incorrect Guess"
+           else putStr ""
+         
+        putStrLn $ show guessed
+        putStrLn $ show intButtIdx
         putStrLn $ show pn'
+        putStrLn $ show usrButtIdx
+        -- putStrLn $ show pn'
         putStrLn $ show (a',b')
-        putStrLn $ show $ isButtPressed context (x',y')
-        putStrLn $ show (x',y')
+        putStrLn ""
+        -- putStrLn $ show $ isButtPressed context (x',y')
+        -- putStrLn $ show (x',y')
         
         send context (do
-                --let (wdt, hgt) = (height context, width context)::(Float,Float)
                 let (wdt, hgt) = (width context, height context)::(Float,Float)                         
                 save()
-                let hRat = 0.4 --3/5  --height ratio
-                    wRat = 0.4 --3/10 --width ratio
+                let hRat = 0.4 --height ratio
+                    wRat = 0.4 --width ratio
                 translate (wdt * wRat, hgt * hRat)
                 
-                fillText(Text.pack $ show $ wdt * wRat,-200,-200)
-                fillText(Text.pack $ show $ hgt * hRat,-200,-250)              
+                -- fillText(Text.pack $ show $ wdt * wRat,-200,-200)
+                -- fillText(Text.pack $ show $ hgt * hRat,-200,-250)              
                 -- fillText(Text.pack $ show (x',y'),-200,-200)
-                fillText(Text.pack $ show (wdt, hgt),-150,-150)
+                -- fillText(Text.pack $ show (wdt, hgt),-150,-150)
 
                 stroke()
                 
@@ -66,7 +76,7 @@ looper context (x',y') (a,b) pn = do
                       clearRect (-(wdt * wRat), -(hgt * hRat), wdt, hgt)
                       let buttCoord = [ drawButton (x*55 - 193,150) y |
                                         (x,y) <- [(0,"u"),(1,"m2"),(2,"M2"),(3,"m3"),(4,"M3"),(5,"P4"),(6,"TT"),
-                                                  (7,"P5"),(8,"m6"),(9,"M6"),(10,"m7"),(11,"8ve")]]
+                                                  (7,"P5"),(8,"m6"),(9,"M6"),(10,"m7"),(11,"M7"),(12,"8ve")]]
                       buttCoord' <- sequence buttCoord
                       let buttIdx = buttonPress buttCoord' (x' - wdt * wRat, y' - hgt * hRat)
 
@@ -148,8 +158,8 @@ buttonPressWorker _ _               = -1
 
 getNotes :: Bool -> (Int, Int) -> IO (Int, Int)
 getNotes True _  = do g <- getStdGen
-                      let (x,g') = randomR (1,11) g
-                      let (y,_ ) = randomR (max 1 (x - 7), min 11 (x + 7)) g'
+                      let (x,g') = randomR (1,10) g
+                      let (y,_ ) = randomR (max 1 (x - 7), min 10 (x + 7)) g'
                       return (x,y)
 getNotes False a = do return a
 
@@ -161,12 +171,13 @@ intToPitch i = let notes = [c, d, e, f, g, a, b, c, d, e, f, g]
                                      --change from writeMidi to play when publish
 playNotes :: (Int,Int) -> IO ()
 playNotes (x,y) = do let z = intToPitch x :+: intToPitch y
-                     writeMidi "randomTest.mid" z --play z
+                     writeMidi "randomTest.mid" z
+                     --play z
 
 intToPitchToButtIdx :: (Int, Int) -> Int
 intToPitchToButtIdx (x,y) = let a = getAbsPitch $ intToPitch x
-                                b = getAbsPitch$ intToPitch y
-                            in abs $ (a - b)
+                                b = getAbsPitch $ intToPitch y
+                            in a - b
 
 getAbsPitch :: Music Pitch -> Int
 getAbsPitch (Prim (Note _ m)) = absPitch m
@@ -182,13 +193,15 @@ getAbsPitch (Prim (Note _ m)) = absPitch m
 --                                 -- in if isJust buttIdx then False else True
 --                                 in (buttCoord, buttIdx)
 
+getButtPressed :: DeviceContext -> (Float, Float) -> Maybe Float
+getButtPressed context (x',y') = let buttCoord = [ (x*55 - 193 + wdt * wRat, 150 + hgt * hRat) | x <- [0..12]]
+                                     wdt = width context
+                                     hgt = height context
+                                     hRat = 0.4
+                                     wRat = 0.4
+                                 in buttonPress buttCoord (x', y')
+              
+                                                                    
 isButtPressed :: DeviceContext -> (Float, Float) -> Bool
-isButtPressed context (x',y') = let buttCoord = [ (x*55 - 193 + wdt * wRat, 150 + hgt * hRat) | x <- [0..11]]
-                                    wdt = width context
-                                    hgt = height context
-                                    hRat = 0.4--3/5
-                                    wRat = 0.4--3/10
-                                    buttIdx = buttonPress buttCoord (x', y')
-                                in if isJust buttIdx then True else False
-                                -- in (buttCoord, buttIdx)
+isButtPressed context (x',y') = if isJust $ getButtPressed context (x',y') then False else True
                                                                                                                                                                                                                                                                                                                          
